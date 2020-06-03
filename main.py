8e8b2f0a36041e3c63bdf36400c8dcf44e72a025
Casper Bøgeskov Hansen <casperbh.96@gmail.com>
2020-06-03 11:41:14 +0200

    Fixed sliders to use float values

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def root():
-    return app.send_static_file('test.html')
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ static/index.js
@@ -0,0 +1,31 @@
+var change1 = function() {
+	$("#output1").text(sepal_length.getValue());
+};
+
+var change2 = function() {
+	$("#output2").text(sepal_width.getValue());
+};
+
+var change3 = function() {
+	$("#output3").text(petal_length.getValue());
+};
+
+var change4 = function() {
+	$("#output4").text(petal_width.getValue());
+};
+
+var sepal_length = $('#sepal_length').slider()
+					.on('slide', change1)
+					.data('slider');
+
+var sepal_width = $('#sepal_width').slider()
+					.on('slide', change2)
+					.data('slider');
+
+var petal_length = $('#petal_length').slider()
+					.on('slide', change3)
+					.data('slider');
+
+var petal_width = $('#petal_width').slider()
+					.on('slide', change4)
+					.data('slider');
