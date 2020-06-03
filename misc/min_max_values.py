18a5c1bbc6471f4b9317e0f425540301a1775ec6
Casper Bøgeskov Hansen <casperbh.96@gmail.com>
2020-06-03 19:10:17 +0200

    Added more packages to requirements.txt and added a file to request the server

--- misc/min_max_values.py
+++ misc/min_max_values.py
@@ -2,9 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import joblib
 
 data_loader = load_iris()
 
